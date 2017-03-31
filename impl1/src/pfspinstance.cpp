3a8adcb1bed6ac08bc5fa9be86290f83b2dd78e7
t00n <kelkununtel@hotmail.com>
2017-03-31 16:25:45 +0200

    [fix] same score ass assistants

--- impl1/src/pfspinstance.cpp
+++ impl1/src/pfspinstance.cpp
@@ -39,12 +39,12 @@ PfspInstance::~PfspInstance()
 
 int PfspInstance::getNbJob()
 {
-    return nbJob;
+  return nbJob;
 }
 
 int PfspInstance::getNbMac()
 {
-    return nbMac;
+  return nbMac;
 }
 
 
@@ -52,150 +52,141 @@ int PfspInstance::getNbMac()
 /* Allow the memory for the processing times matrix : */
 void PfspInstance::allowMatrixMemory(int nbJ, int nbM)
 {
-    processingTimesMatrix.resize(nbJ);          // 1st dimension
+  processingTimesMatrix.resize(nbJ+1);          // 1st dimension
 
-    for (int cpt = 0; cpt < nbJ; ++cpt) {
-        processingTimesMatrix[cpt].resize(nbM); // 2nd dimension
-    }
+  for (int cpt = 0; cpt < nbJ+1; ++cpt)
+    processingTimesMatrix[cpt].resize(nbM+1); // 2nd dimension
 
-    dueDates.resize(nbJ);
-    priority.resize(nbJ);
+	dueDates.resize(nbJ+1);
+	priority.resize(nbJ+1);
 }
 
 
 long int PfspInstance::getTime(int job, int machine)
 {
-    if ((job < 0) || (job >= nbJob) || (machine < 0) || (machine >= nbMac))
-      cerr    << "ERROR. file:pfspInstance.cpp, method:getTime. Out of bound. job=" << job
+  if (job == 0)
+    return 0;
+  else
+  {
+    if ((job < 1) || (job > nbJob) || (machine < 1) || (machine > nbMac))
+      cout    << "ERROR. file:pfspInstance.cpp, method:getTime. Out of bound. job=" << job
           << ", machine=" << machine << std::endl;
 
     return processingTimesMatrix[job][machine];
+  }
 }
 
 
 /* Read the instance from file : */
 bool PfspInstance::readDataFromFile(char * fileName)
 {
-    bool everythingOK = true;
-    int j, m; // iterators
-    long int readValue;
-    string str;
-    ifstream fileIn;
-    char * aux2;
-    char fileNameOK[100] = "";
+	bool everythingOK = true;
+	int j, m; // iterators
+	long int readValue;
+	string str;
+	ifstream fileIn;
+	char * aux2;
+	char fileNameOK[100] = "";
 
-    aux2 = (strrchr(fileName, '/'));
+	aux2 = (strrchr(fileName, '/'));
 
-    if (aux2 == NULL)
-        aux2 = fileName;
-    else
-        aux2 += 1;
+	if (aux2 == NULL)
+		aux2 = fileName;
+	else
+		aux2 += 1;
 
-    strcat(fileNameOK, aux2);
+	strcat(fileNameOK, aux2);
 
-    cerr << "name : " << fileNameOK << endl;
-    cerr << "file : " << fileName << endl;
+	cout << "name : " << fileNameOK << endl;
+	cout << "file : " << fileName << endl;
 
-    fileIn.open(fileName);
+	fileIn.open(fileName);
 
-    if ( fileIn.is_open() ) {
-        cerr << "File " << fileName << " is now open, start to read..." << std::endl;
+	if ( fileIn.is_open() ) {
+        cout << "File " << fileName << " is now open, start to read..." << std::endl;
 
-        fileIn >> nbJob;
-        cerr << "Number of jobs : " << nbJob << std::endl;
-        fileIn >> nbMac;
-        cerr << "Number of machines : " << nbMac << std::endl;
-        cerr << "Allow memory for the matrix..." << std::endl;
-        allowMatrixMemory(nbJob, nbMac);
-        cerr << "Memory allowed." << std::endl;
-        cerr << "Start to read matrix..." << std::endl;
+		fileIn >> nbJob;
+        cout << "Number of jobs : " << nbJob << std::endl;
+		fileIn >> nbMac;
+        cout << "Number of machines : " << nbMac << std::endl;
+        cout << "Allow memory for the matrix..." << std::endl;
+		allowMatrixMemory(nbJob, nbMac);
+        cout << "Memory allowed." << std::endl;
+        cout << "Start to read matrix..." << std::endl;
 
-        for (j = 0; j < nbJob; ++j)
-        {
-            for (m = 0; m < nbMac; ++m)
-            {
-                fileIn >> readValue; // The number of each machine, not important !
-                fileIn >> readValue; // Process Time
+		for (j = 1; j <= nbJob; ++j)
+		{
+			for (m = 1; m <= nbMac; ++m)
+			{
+				fileIn >> readValue; // The number of each machine, not important !
+				fileIn >> readValue; // Process Time
 
-                processingTimesMatrix[j][m] = readValue;
-            }
-        }
+				processingTimesMatrix[j][m] = readValue;
+			}
+		}
         fileIn >> str; // this is not read
 
-        for (j = 0; j < nbJob; ++j)
-        {
-            fileIn >> readValue; // -1
-            fileIn >> readValue;
-            dueDates[j] = readValue;
-            fileIn >> readValue; // -1
-            fileIn >> readValue;
+		for (j = 1; j <= nbJob; ++j)
+		{
+			fileIn >> readValue; // -1
+			fileIn >> readValue;
+			dueDates[j] = readValue;
+			fileIn >> readValue; // -1
+			fileIn >> readValue;
             priority[j] = readValue;
-        }
+		}
 
-        cerr << "All is read from file." << std::endl;
-        fileIn.close();
-    }
-    else
-    {
-        cerr    << "ERROR. file:pfspInstance.cpp, method:readDataFromFile, "
-                << "error while opening file " << fileName << std::endl;
+        cout << "All is read from file." << std::endl;
+		fileIn.close();
+	}
+	else
+	{
+		cout    << "ERROR. file:pfspInstance.cpp, method:readDataFromFile, "
+				<< "error while opening file " << fileName << std::endl;
 
-        everythingOK = false;
-    }
+		everythingOK = false;
+	}
 
-    return everythingOK;
+	return everythingOK;
 }
 
 
 /* Compute the weighted tardiness of a given solution */
-long int PfspInstance::computeScore(vector<int> & sol)
+long int PfspInstance::computeScore(vector< int > & sol)
 {
-    int j, m;
-    int jobNumber;
-    long int wct;
+	int j, m;
+	int jobNumber;
+	long int score;
 
-    /* We need end times on previous machine : */
-    vector< long int > previousMachineEndTime(nbJob);
-    /* And the end time of the previous job, on the same machine : */
-    long int previousJobEndTime;
+	/* We need end times on previous machine : */
+	vector< long int > previousMachineEndTime ( nbJob + 1 );
+	/* And the end time of the previous job, on the same machine : */
 
-    /* 1st machine : */
-    previousMachineEndTime[0] = 0;
-    for (j = 1; j < nbJob; ++j)
-    {
-        jobNumber = sol[j];
-        previousMachineEndTime[j] = previousMachineEndTime[j-1] + processingTimesMatrix[jobNumber][0];
-    }
+	/* 1st machine : */
+	previousMachineEndTime[0] = 0;
+	for ( j = 1; j <= nbJob; ++j )
+	{
+		jobNumber = sol[j-1];
+		previousMachineEndTime[j] = previousMachineEndTime[j-1] + processingTimesMatrix[jobNumber][1];
+	}
 
-    /* others machines : */
-    for (m = 1; m < nbMac; ++m)
-    {
-        previousMachineEndTime[0] +=
-                processingTimesMatrix[sol[0]][m];
-        previousJobEndTime = previousMachineEndTime[0];
+	/* others machines : */
+	for ( m = 2; m <= nbMac; ++m )
+	{
+		for ( j = 1; j <= nbJob; ++j )
+		{
+			jobNumber = sol[j-1];
 
+            previousMachineEndTime[j] = std::max(
+                previousMachineEndTime[j],
+                previousMachineEndTime[j-1]
+            ) + processingTimesMatrix[jobNumber][m];
+		}
+	}
 
-        for (j = 1; j < nbJob; ++j)
-        {
-            jobNumber = sol[j];
+	score = 0;
+	for (j = 0; j < nbJob; ++j)
+	    score += previousMachineEndTime[j+1] * priority[sol[j]];
 
-            if (previousMachineEndTime[j] > previousJobEndTime)
-            {
-                previousMachineEndTime[j] = previousMachineEndTime[j] + processingTimesMatrix[jobNumber][m];
-                previousJobEndTime = previousMachineEndTime[j];
-            }
-            else
-            {
-                previousJobEndTime += processingTimesMatrix[jobNumber][m];
-                previousMachineEndTime[j] = previousJobEndTime;
-            }
-        }
-    }
-
-    wct = 0;
-    for (j = 0; j < nbJob; ++j) {
-        wct += previousMachineEndTime[j] * priority[sol[j]];
-    }
-
-    return wct;
+	return score;
 }
