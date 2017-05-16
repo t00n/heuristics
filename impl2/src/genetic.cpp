1b3240cdf28ad5245d1a628968f38ee1da4b94f6
t00n <kelkununtel@hotmail.com>
2017-05-16 20:17:47 +0200

    [enh] more mutations and local search

--- impl2/src/genetic.cpp
+++ impl2/src/genetic.cpp
@@ -10,7 +10,6 @@ void Genetic::solve() {
 	auto t = clock();
 	size_t Ps = 20;
 	float Pm = 0.8;
-	int Max_Stuck = 20;
 	int TLS = 5;
 	int Stuck_loop = 0;
 	// step 2 : init pop randomly
@@ -18,13 +17,6 @@ void Genetic::solve() {
 	// step 3 : find best solution
 	PfspSolution best_solution;
 	double best_score = std::numeric_limits<double>::infinity();
-	for (auto sol : pop) {
-		double score = this->instance.computeScore(sol);
-		if (score < best_score) {
-			best_score = score;
-			best_solution = sol;
-		}
-	}
 	std::uniform_int_distribution<> dis(0, Ps - 1);
 	do {
 		// Step 4 : apply Step 5-7 Ps times
@@ -34,14 +26,17 @@ void Genetic::solve() {
 			do {
 				father = dis(gen);
 			} while (father == mother);
+			// Step 5 : crossover to create child
 			auto child = OAcrossover(this->instance, pop[mother], pop[father]);
 			auto mother_score = instance.computeScore(pop[mother]);
 			auto father_score = instance.computeScore(pop[father]);
 			auto child_score = instance.computeScore(child);
+			// Step 6 : use local search if stuck for too long
 			if ((child_score < mother_score or child_score < father_score) and Stuck_loop > TLS) {
-				child = RZ(instance, child);
+				child = iRZ(instance, child);
 				child_score = instance.computeScore(child);
 			}
+			// Step 7 : replace worst parent by child
 			if (child_score < mother_score and mother_score < father_score) {
 				pop[father] = child;
 			}
@@ -69,7 +64,7 @@ void Genetic::solve() {
 		// Step 9 ; mutation
 		for (size_t i = 0; i < Ps * Pm; ++i) {
 			auto r = dis(gen);
-			pop[r] = destruction_construction(instance, pop[r], 5);
+			pop[r] = destruction_construction(instance, pop[r], 8);
 		}
 	} while (float(clock() - t) / CLOCKS_PER_SEC < this->timeout);
 	this->solution = best_solution;
